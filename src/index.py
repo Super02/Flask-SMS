d6bdf9551ed91def08fe82045d59e6e7414ee7d6
BuildTools <unconfigured@null.spigotmc.org>
2019-10-20 14:28:02 +0200

    OOfmaster

--- src/index.py
+++ src/index.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, render_template, jsonify
 from flask_httpauth import HTTPBasicAuth
-from twilio.rest import Client 
-from twilio.base.exceptions import TwilioRestException
+import nexmo
 from random import choice
 from string import digits
 from os import getenv
@@ -9,10 +8,10 @@ from redis import Redis
 
 app = Flask(__name__)
 auth = HTTPBasicAuth()
-account_sid = getenv("account_sid")
-auth_token = getenv('auth_token')
-client = Client(account_sid, auth_token)
-redis = Redis().from_url(getenv("REDIS_URL"))
+secret = getenv("secret")
+key = getenv("key")
+client = nexmo.Client(key=key, secret=secret)
+redis = Redis().from_url("redis://SG-SMS-26975.servers.mongodirector.com:6379")
 
 @auth.verify_password
 def verify_password(username, password):
@@ -24,7 +23,7 @@ def verify_password(username, password):
 def fix_number(raw: str):
 	raw = raw.replace(" ", "")
 	raw = raw.replace("+45", "")
-	return "+45{}".format(raw)
+	return "{}".format(raw)
 
 def send_sms(src:str, dst:str, text:str, key=None):
 	if not key is None:
@@ -37,19 +36,57 @@ def send_sms(src:str, dst:str, text:str, key=None):
 		if len(text) == 0 or len(text.encode()) > 140:
 			return jsonify({"Error": "Invalid message length"}), 400
 		try:
-			message = client.messages.create(body=text, from_="a31688852", to=fix_number(dst))
-			print(message.sid)
+			message =client.send_message({'from': fix_number(src),'to': "45"+fix_number(dst),'text': text,})
 			if key is not None:
 				redis.lrem("sms_keys", 0, key)
-		except TwilioRestException:
+		except Exception: #FIX LIGE OOF
 			return jsonify({"Error": "Source and Destination cannot be the same"}), 400
 		redis.lpush("log", f"{src} => {dst} | Key: {key} | Text: {text}")
 		return render_template("result.html", msg=message, admin=(key is None))
+	else:
+		message =client.send_message({'from': fix_number(src),'to': "45"+fix_number(dst),'text': text,})
+		if(message["messages"][0]["status"] == "0"):
+			redis.lpush("log", "Message sent \nFrom: {}\nTo: {}\nText: {}".format(fix_number(src),fix_number(dst),text))
+		else:
+			redis.lpush("log", f"Message failed with error: {message ['messages'][0]['error-text']}")
+			print(f"Message failed with error: {message ['messages'][0]['error-text']}")
 
 
 def generate_random_key(length: int):
 	return "".join(choice(digits) for _ in range(length))
 
+@app.route('/admin', methods=['GET', 'POST'])
+@auth.login_required
+def admin_panel():
+	if request.method == "POST":
+		rcv = fix_number(request.form.get('rcv'))
+
+		keys = redis.lrange("sms_keys", 0, -1)
+		key = generate_random_key(4)
+		while key.encode() in keys:
+			key = generate_random_key(4)
+			
+		if (len(rcv) != 11 or len(rcv) != 11) and len(rcv) != 3:
+			return jsonify({"Error": "Phone number isn't 8 numbers long."}), 400
+
+		text = "Your one time key is: {}".format(key)
+
+		try:
+			if len(rcv) != 3:
+				message =client.send_message({'from': "69696969",'to': "45"+fix_number(rcv),'text': text,})
+			
+			redis.lpush("sms_keys", key)
+			
+			if len(rcv) == 3:
+				redis.lpush("log", f"Generated anonymous key ({key})")
+				return jsonify({"key": key})
+		except:
+			return jsonify({"Error": "Unknown Error!"})
+
+		redis.lpush("log", f"Generated key for {rcv} ({key})")
+		return render_template("result.html", msg=message_data, admin=True)
+
+	return render_template("admin.html")
 
 
 @app.route('/admin/sms', methods=['GET', 'POST'])
@@ -65,7 +102,7 @@ def admin_sms():
 	return render_template("send_sms.html", admin=True)
 @app.route('/sms', methods=['GET', 'POST'])
 def sms():
-	if request.method == "POST":
+	if request.method == "GET":
 		src = fix_number(request.form.get('src'))
 		dst = fix_number(request.form.get('dst'))
 		text = request.form.get('text')
