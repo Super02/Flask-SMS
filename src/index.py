c9174c51359585e4f46bba8db415f4c1e6f11589
BuildTools <unconfigured@null.spigotmc.org>
2019-10-24 16:36:44 +0200

    Added a lot of features including an actually working SMS sender. YAY!

--- src/index.py
+++ src/index.py
@@ -41,7 +41,7 @@ def listen_receipts():
 	for x in range(25):
 		time.sleep(1)
 		print(f"{x}/25 Waiting for receipt " + str(redis.get("receipt")))
-		if(str(redis.get("receipt")) != ""): break
+		if(str(redis.get("receipt")) != "b''"): break
 	sent=redis.get("receipt").decode()
 	redis.set("receipt", "")
 	return json.loads(sent.replace("'", "\""))
@@ -51,6 +51,31 @@ def sendLog(logdata:str): #Fix pls
 	dt = datetime.now()
 	redis.lpush("log", "Browser: " + request.headers.get('User-Agent') + " | " + "IP: " + request.remote_addr + " | " + "Timestamp: " + str(dt) + " | " + logdata)
 
+def send_message(src:str, dst:str, text:str, key:str):
+	src=fix_number(src)
+	dst=fix_number(dst)
+	if(len(src) != 10):
+		return "Source number is not 8 numbers long."
+	if(len(dst) != 10):
+		return "Destination number is not 8 numbers long."
+	if(len(text) == 0 or len(text) > 140):
+		return "Message either too small or too long."
+	if(len(key) == 0):
+		return "No key entered."
+	keys=redis.lrange("sms_keys", 0, -1)
+	if(key.encode() in keys):
+		try:
+			message = client.send_message({'from': src,'to': dst,'text': text})
+			if(key != None):
+				redis.lrem("sms_keys", 0, key)
+			return True
+		except Exception as e:
+			print(e)
+			traceback.print_exc()
+			return "Unknown error. Check console for more info."
+	else:
+		return "Key does not exist."
+
 @auth.verify_password
 def verify_password(username, password):
 	if (username, password) == ("admin", getenv("admin_pass")):
@@ -58,8 +83,21 @@ def verify_password(username, password):
 	else:
 		return False
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home(): 
+	if(request.method == "POST"):
+		src = request.form.get('src')
+		dst = request.form.get('dst')
+		key = request.form.get('key')
+		text = request.form.get('text')
+		message = send_message(src,dst,text,key)
+		if(message == True):
+			special=True
+			if(isInt(fix_number(src))):
+				special=False
+			return render_template("receipt.html", data=listen_receipts(), admin=True, key=key, special=special)
+		else:
+			return render_template("showtext.html", title="Error",text=message)
 	return render_template("index.html")	
 
 @app.route('/admin', methods=['GET', 'POST'])
@@ -78,15 +116,26 @@ def admin_panel():
 			return render_template("showtext.html", title="Error!", text="Phone number is not 8 numbers long.")
 		message = "Your one time key is: {} \nuse it here: {}".format(key, url_for('admin_panel')) #Make sure this actually works
 		try:
-			message = client.send_message({'from': "SMSService",'to': reciever,'text': message,})
+			message = client.send_message({'from': "SMSService",'to': reciever,'text': message})
 			sendLog(f"Generated 1 key for {reciever} ({key})") # Might wanna check how it works with sendlog
-			data=listen_receipts()
-			return render_template("receipt.html", from_=data["to"], to=data["msisdn"], status=data["status"], code=data["err-code"], uid=data["messageId"], price=data["price"], admin=True, key=key) # **Make sure this waits for receipt**
+			return render_template("receipt.html", data=listen_receipts(), admin=True, key=key, special=True)
 		except Exception as e:
 			print("Error! " + str(e))
 			traceback.print_exc()
 			return jsonify({"Error": "An unknown error occured. Please contact us for more info! "})
 	return render_template("admin_panel.html")
+@app.route('/admin/sms_keys', methods=['GET', 'POST'])
+@auth.login_required
+def sms_keys():
+	return render_template("showtext.html", title="Error!", text="This page is under construction.")
+	if(request.method == "POST"):
+		redis.lset("sms_keys", 0, "DELETED8b57705a-f65c-11e9-802a-5aa538984bd8")
+		redis.lrem("sms_keys", 1, "DELETED8b57705a-f65c-11e9-802a-5aa538984bd8")
+	else:
+		dropdown=""
+		for i,x in enumerate(redis.lrange("sms_keys", 0, -1)):
+			dropdown+=f"<a class=\"dropdown-item\" href=\"#{i}\">{x.decode()}</a>"
+		return render_template("sms_keys.html", dropdown=dropdown)
 
 @app.route("/DLR-receipts", methods=['GET', 'POST'])
 def DLRReceipts():
