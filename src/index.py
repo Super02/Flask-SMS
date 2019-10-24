946ed7b39aafce4a71799fa31cea8a151c8eb936
Super02 <othilade@gmail.com>
2019-10-24 13:16:20 +0200

    Fix

--- src/index.py
+++ src/index.py
@@ -74,7 +74,7 @@ def admin_panel():
 		try:
 			message = client.send_message({'from': "SMSService",'to': reciever,'text': message,})
 			sendLog(f"Generated 1 key for {reciever} ({key})") # Might wanna check how it works with sendlog
-			data=listen_receipts()
+			data=json.loads(listen_receipts())
 			return render_template("receipt.html", from_=data["to"], to=data["msisdn"], status=data["status"], code=data["err-code"], uid=data["messageId"], price=data["price"], admin=True, key=key) # **Make sure this waits for receipt**
 		except Exception as e:
 			print("Error! " + str(e))
